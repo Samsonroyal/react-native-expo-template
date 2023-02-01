850430e22a672b1ca08008095f454b43f1fe26a9
Were Samson Bruno <werefast2000@gmail.com>
2023-02-01 03:51:13 +0300

    added variables to connect app to firebase auth

--- template-with-firebase-auth-flow/src/navigation/AppNavigator.js
+++ template-with-firebase-auth-flow/src/navigation/AppNavigator.js
@@ -17,13 +17,13 @@ import Loading from "../screens/utils/Loading";
 
 // Better put your these secret keys in .env file
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "process.env.API_KEY",
+  authDomain: "process.env.AUTH_DOMAIN",
+  databaseURL: "process.env.DATABASE_URL",
+  projectId: "process.env.PROJECT_ID",
+  storageBucket: "process.env.STORAGE_BUCKET",
+  messagingSenderId: "process.env.MESSAGING_SENDER_ID",
+  appId: "process.env.APP_ID",
 };
 if (getApps().length === 0) {
   initializeApp(firebaseConfig);
